1c8444b11739876aeb51dc76dff5cdb34fdc1e65
luv1593 <lucas.verrilli@northwestern.edu>
2021-07-26 14:09:44 -0500

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -10,7 +10,7 @@ comparison () {
 
   EMAIL+="$newline "
 
-  EMAIL+="difference between latest tag and '$1':"
+  EMAIL+=" 'difference between latest tag and '$1':', "
 
   #If there is no branch that matched a name in the QA check list then it says there is no match
   #If a match is found the branch is compared to the latest version
