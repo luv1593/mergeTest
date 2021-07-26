db61335d696178bccd53c0f3af67b9753b1acd6c
luv1593 <lucas.verrilli@northwestern.edu>
2021-07-26 10:37:12 -0500

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -3,7 +3,7 @@
 comparison () {
 
 
-  curl -i -X POST -H "Content-Type: application/json" -d "{\"text\":\"hello\"}" ${env.TEAMS_WEBHOOK_URL}
+  curl -i -X POST -H "Content-Type: application/json" -d "{\"text\":\"test\"}" ${env.TEAMS_WEBHOOK_URL}
 
   EMAIL+='--------------------------- latest vs '$1' ---------------------------------'
 
