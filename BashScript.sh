a91adae901d429f1ecfd07d86e6d22f19e075243
luv1593 <lucas.verrilli@northwestern.edu>
2021-07-27 14:25:05 -0500

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -237,8 +237,8 @@ do
     "@type": "MessageCard",
     "themeColor": "0076D7",
     "summary": "hello",
-    "title": "'$i'",
-    "text": "'$EMAIL'",
+    "title": " REPO GOES HERE ",
+    "text": "EMAIL DATA HERE",
     "potentialAction": [{
         "@type": "ActionCard",
         "name": "Open log",
