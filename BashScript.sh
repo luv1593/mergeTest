5474af076a9a1d403b6acf7bf47a2764ce21c38c
luv1593 <lucas.verrilli@northwestern.edu>
2021-08-13 09:27:35 -0500

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -32,7 +32,7 @@ comparison () {
       NOTIFICATION+="<p style='color:red'>⛔ $(git diff --stat-graph-width=1 $disc..$1 | tail -1)  </p>"
 
       echo "WORKS?"
-      if [ "$1" = "$DEVSTR" ]:
+      if [ "$1" = "$DEVSTR" ];
       then
 
 
